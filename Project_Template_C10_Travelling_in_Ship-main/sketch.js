717662f6e97754872b1be10b7f0ee455933cbc6a
Rheacode <88169842+Rheacode@users.noreply.github.com>
2022-04-13 15:31:43 -0400

    Add files via upload

---
+++ Project_Template_C10_Travelling_in_Ship-main/sketch.js
@@ -0,0 +1,17 @@
+
+function preload(){
+  seaImg = loadImage("sea.png")
+  ship1 = loadAnimation("ship-1.png, ship-2.png, ship-3.png ship-4.png")
+
+}
+
+function setup(){
+  createCanvas(400,400);
+  
+}
+
+function draw() {
+  background("blue");
+sea.addImage(seaImg)
+ 
+}
